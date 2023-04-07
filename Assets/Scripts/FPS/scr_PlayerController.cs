2c6fef5eba66b03b275402538416b209973b96fd
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2023-04-06 23:49:10 -0700

    Imported Poly War Pack

---
+++ Assets/Scripts/FPS/scr_PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class scr_PlayerController {
+    enum CharacterState
+    {
+        Idle,
+        Walking,
+        Running,
+        Jumping,
+        Standing,
+        Crouching,
+        Proning,
+        Aiming,
+        Shooting
+
+    }
+}
